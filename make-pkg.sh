b3a1f2bd0989077bd96606210e967a09c79d9e04
Gaël PORTAY <gael.portay@gmail.com>
2026-01-07 17:50:53 +0100

    make-{deb,pkg,rpm}.sh: use double-dashed command --shell
    This fixes commit 29e4c36f0047bb0505c83b4a71be8917a4d2406e.

--- make-pkg.sh
+++ make-pkg.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S cqfd -b pkg shell
+#!/usr/bin/env -S cqfd -b pkg --shell
 set -e
 makepkg --force --skipchecksums "$@"
 shellcheck --shell=bash --exclude=SC2034,SC2154,SC2164 PKGBUILD
