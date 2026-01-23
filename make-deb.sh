14a2d76701e50190401d582daeadad0b7d1f661f
Gaël PORTAY <gael.portay@gmail.com>
2026-01-23 05:39:08 +0100

    cqfd: add option --yes
    This adds option --yes to assume yes if prompting to user.
    
    Fixes:
    
    	$ ./make-deb.sh
    	cqfd: fatal: The docker image doesn't exist, launch 'cqfd --init' to create it

--- make-deb.sh
+++ make-deb.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S "CQFD_EXTRA_RUN_ARGS=--volume ${HOME}:${HOME}" cqfd6 --build deb /bin/bash
+#!/usr/bin/env -S "CQFD_EXTRA_RUN_ARGS=--volume ${HOME}:${HOME}" cqfd6 --build deb --yes /bin/bash
 set -e
 dpkg-buildpackage -us -uc "$@"
 lintian ../cqfd*.dsc ../cqfd*.deb
