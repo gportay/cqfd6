bc7cad69239ff87561b2e6bdf0044e9826a73ba3
Gaël PORTAY <gael.portay@gmail.com>
2026-01-23 08:57:35 +0100

    make-{deb,pkg,rpm}.sh: use long option --build
    This fixes commit a34d47f56d0c6c26e051d303bdbb81c832005c98.

--- make-deb.sh
+++ make-deb.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S "CQFD_EXTRA_RUN_ARGS=--volume ${HOME}:${HOME}" cqfd -b deb --shell
+#!/usr/bin/env -S "CQFD_EXTRA_RUN_ARGS=--volume ${HOME}:${HOME}" cqfd --build deb --shell
 set -e
 dpkg-buildpackage -us -uc "$@"
 lintian ../cqfd*.dsc ../cqfd*.deb
